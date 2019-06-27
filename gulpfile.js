019551c97ec5914157452bac5214fc9a5b9b475a
Mitchell Jones <mitch@mitchelljones.com>
2019-06-27 14:38:27 -0700

    gulp implemented

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 const browsersync = require("browser-sync").create();
+
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const terser = require('gulp-terser');
@@ -8,12 +9,11 @@ const mincss = require('gulp-clean-css');
 function compileStylus() {
     return gulp.src('public/**/*.styl', {base: './'})
         .pipe(stylus())
-        .pipe(rename((path) => {path.basename = path.basename + '-min'}))
         .pipe(gulp.dest('.'));
 }
 
 function minifyJS() {
-    return gulp.src(['public/**/*.js', '!public/projects/dsgame/code/nodeservercode.js'], {base: './'})
+    return gulp.src(['public/**/*.js', '!public/**/*-min.js', '!public/projects/dsgame/code/nodeservercode.js'], {base: './'})
         .pipe(terser())
         .pipe(rename((path) => {
             path.basename = path.basename + '-min';
@@ -22,41 +22,19 @@ function minifyJS() {
 }
 
 function minifyCSS() {
-    return gulp.src('public/**/*.css', {base: './'})
+    return gulp.src(['public/**/*.css', '!public/**/*-min.css'], {base: './'})
         .pipe(mincss())
         .pipe(rename((path) => {
             path.basename = path.basename + '-min';
         }))
         .pipe(gulp.dest('.'));
 }
-
-gulp.task('build', () => {compileStylus();minifyJS();minifyCSS();});
 gulp.task('stylus', compileStylus);
 gulp.task('minifyJS', minifyJS);
 gulp.task('minifyCSS', minifyCSS);
 
+gulp.task('build', gulp.series('stylus', 'minifyJS', 'minifyCSS'));
+
 gulp.task('watch', function() {
     gulp.watch('js/*.js', ['lint', 'scripts']);
 });
-
-function browserSync(done) {
-    browsersync.init({
-       server: {
-           baseDir: './'
-       }
-    });
-    done();
-}
-
-function bsReload(done) {
-    browsersync.reload();
-    done();
-}
-
-function watchFiles() {
-    gulp.watch('./styl/**/*', css);
-    gulp.watch(['.js/**/*.js', '!.js/*.min.js'], js);
-    gulp.watch('./**/*.html', bsReload);
-}
-
-gulp.task('dev', gulp.parallel(watchFiles, browserSync));
