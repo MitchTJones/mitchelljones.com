a8f64ba037407a4ec8169fb0b480b44fbbe9b3dd
Mitchell Jones <mitch@mitchelljones.com>
2019-06-27 12:43:06 -0700

    implement gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,38 @@ const browsersync = require("browser-sync").create();
 const gulp = require('gulp');
 const stylus = require('gulp-stylus');
 const terser = require('gulp-terser');
+const rename = require('gulp-rename');
+const mincss = require('gulp-clean-css');
 
-gulp.task('stylus', function() {
-    return gulp.src('public/**/*.styl')
+function compileStylus() {
+    return gulp.src('public/**/*.styl', {base: './'})
         .pipe(stylus())
-        .pipe(gulp.dest('public/css'))
-});
+        .pipe(rename((path) => {path.basename = path.basename + '-min'}))
+        .pipe(gulp.dest('.'));
+}
 
-gulp.task('minify', function() {
+function minifyJS() {
     return gulp.src('public/**/*.js', {base: './'})
         .pipe(terser())
+        .pipe(rename((path) => {
+            path.basename = path.basename + '-min';
+        }))
         .pipe(gulp.dest('.'));
-});
+}
+
+function minifyCSS() {
+    return gulp.src('public/**/*.css', {base: './'})
+        .pipe(mincss())
+        .pipe(rename((path) => {
+            path.basename = path.basename + '-min';
+        }))
+        .pipe(gulp.dest('.'));
+}
+
+gulp.task('build', () => {compileStylus();minifyJS();minifyCSS();});
+gulp.task('stylus', compileStylus);
+gulp.task('minifyJS', minifyJS);
+gulp.task('minifyCSS', minifyCSS);
 
 gulp.task('watch', function() {
     gulp.watch('js/*.js', ['lint', 'scripts']);
